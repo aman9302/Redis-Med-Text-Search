e6cf86d509041e926f9507daccad18ccbc36f537
aman9302 <117447588+aman9302@users.noreply.github.com>
2023-07-22 12:59:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import requests
 from sentence_transformers import SentenceTransformer
 from rank_bm25 import BM25Okapi
@@ -9,17 +8,28 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 class RedisTextSearch:
-    def __init__(self, url_list):
+    def __init__(self, api_url):
         self.redis_server = redis.Redis()
-        self.texts = self.load_texts_from_url(url_list)
+        self.api_url = api_url
+        self.texts = self.load_texts_from_url()
         self.bm25_model = None
 
-    def load_texts_from_url(self, url_list):
+    def load_texts_from_url(self):
         texts = []
-        for url in url_list:
+        file_urls = self.get_github_files()
+        for url in file_urls:
             text = self.fetch_text_from_url(url)
             texts.append(text)
         return texts
+
+    def get_github_files(self):
+        response = requests.get(self.api_url)
+        if response.status_code == 200:
+            data = response.json()
+            urls = [item['download_url'] for item in data]
+            return urls
+        else:
+            raise ValueError(f"Failed to fetch file URLs from GitHub API. Status code: {response.status_code}")
 
     def fetch_text_from_url(self, url):
         response = requests.get(url)
@@ -61,16 +71,6 @@ class RedisTextSearch:
 
         return similar_texts
 
-# Fetch file URLs from the GitHub repository folder using the GitHub API
-def get_github_files(api_url):
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        data = response.json()
-        urls = [item['download_url'] for item in data]
-        return urls
-    else:
-        raise ValueError(f"Failed to fetch file URLs from GitHub API. Status code: {response.status_code}")
-
 # GitHub repository details
 github_repo = 'aman9302/Redis-Med-Text-Search'
 folder_path = 'mimic_case_data_redis'
@@ -78,10 +78,7 @@ folder_path = 'mimic_case_data_redis'
 # GitHub API endpoint to get the contents of a repository folder
 api_url = f'https://api.github.com/repos/{github_repo}/contents/{folder_path}'
 
-# Get file URLs
-file_urls = get_github_files(api_url)
-
-redis_app = RedisTextSearch(file_urls)
+redis_app = RedisTextSearch(api_url)
 redis_app.store_original_texts()
 redis_app.store_embeddings()
 
@@ -89,3 +86,9 @@ redis_app.store_embeddings()
 def index():
     if request.method == 'POST':
         query_text = request.form['query']
+        results = redis_app.search_bm25_redis(query_text)
+        return render_template('index.html', results=results, query=query_text)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
