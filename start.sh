dc79544be3efca7ff7d7e4c8f2223b8d7617a7c0
aman9302 <117447588+aman9302@users.noreply.github.com>
2023-07-23 13:20:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,22 +3,17 @@
 # Exit early on errors
 set -eu
 
-# Python buffers stdout
-export PYTHONUNBUFFERED=true
-
 # Install Python 3 virtual env
 VIRTUALENV=.data/venv
 
-if [ ! -d $VIRTUALENV ]; then
-  python3 -m venv $VIRTUALENV
-fi
-
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-fi
-
 # Install the requirements
-$VIRTUALENV/bin/pip install -r requirements.txt
+pip install -r requirements.txt
 
-# Run the app
-$VIRTUALENV/bin/python3 app.py
+# Start the Redis server
+redis-server --port 6379 &
+
+# Wait for Redis server to start (add any necessary delay if needed)
+sleep 5
+
+# Start the Streamlit app
+streamlit run --server.port 8501 app.py
